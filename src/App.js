d230a433a67a1442ea3c6212d4c5d925317326b4
James Morris <blondfro@gmail.com>
2022-02-09 17:57:20 -0600

    implemented basic banner component with simple styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Banner from "./Banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <Banner />
     </div>
   );
 }
---
+++ src/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Banner.css";
+
+function Banner() {
+    return (
+        <div className="app-title-section">
+            <h1>Weather Dashboard</h1>
+        </div>
+    )
+}
+
+export default Banner;
